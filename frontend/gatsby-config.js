85445612bf789f6dff0d345475af8cbfff2c0cb1
JohnAlbin <virtually.johnalbin@gmail.com>
2019-07-15 14:19:54 -0500

    Update gatsby-plugin-manifest config

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -51,12 +51,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-drupal-umami`,
+        short_name: `umami`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     }
