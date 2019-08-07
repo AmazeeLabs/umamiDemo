7109f4e0a8d9622d8ef9ef81571d17dbf57ec966
JohnAlbin <virtually.johnalbin@gmail.com>
2019-08-07 16:27:57 +0800

    Remove gatsby-plugin-manifest and gatsby-plugin-offline

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -46,20 +46,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-drupal-umami`,
-        short_name: `umami`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `standalone`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ]
 };
